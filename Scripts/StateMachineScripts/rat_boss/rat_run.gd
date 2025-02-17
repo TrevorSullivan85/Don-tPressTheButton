ff82f106866b0b992d3a03aa547f6d11f70d3e6d
Trevor Sullivan <trevors1775@gmail.com>
2025-02-16 21:49:52 -0500

    basic rat movement working!

--- Scripts/StateMachineScripts/rat_boss/rat_run.gd
+++ Scripts/StateMachineScripts/rat_boss/rat_run.gd
@@ -18,25 +18,31 @@ func process_physics(delta:float) -> State:
 	
 	var movement = move_speed
 	var direction = 0
+	print(keep_running)
 	if keep_running:
-		if player.global_position.x - self.global_position.x > 0:
+		print(player.global_position.x - parent.global_position.x < 65 and player.global_position.x - parent.global_position.x > -65)
+		if player.global_position.x - parent.global_position.x < 65 and player.global_position.x - parent.global_position.x > -65:
+			direction = 0
+		elif player.global_position.x - parent.global_position.x > 0:
 			direction = 1
 		else:
 			direction = -1
 		movement *= direction
 	else:
 		movement = 0;
-	
-	if movement != 0:
+	print(player.global_position.y)
+	print(movement)
+	if movement != 0 or player.global_position.y < -5:
 		if movement < 0:
 			parent.get_node("AnimatedSprite2D").flip_h = true
 		else:
 			parent.get_node("AnimatedSprite2D").flip_h = false
 	else:
+		print("Idle State Started")
 		return idle_state
 	
 	
-		parent.velocity.x = movement
+	parent.velocity.x = movement
 	parent.move_and_slide()
 		
 	return null

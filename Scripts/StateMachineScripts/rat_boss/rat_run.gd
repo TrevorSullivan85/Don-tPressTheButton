c4a3103d82dd34f06c77804439012860b3e04d62
Trevor Sullivan <trevors1775@gmail.com>
2025-02-16 22:17:31 -0500

    changes to rat boss

--- Scripts/StateMachineScripts/rat_boss/rat_run.gd
+++ Scripts/StateMachineScripts/rat_boss/rat_run.gd
@@ -18,9 +18,9 @@ func process_physics(delta:float) -> State:
 	
 	var movement = move_speed
 	var direction = 0
-	print(keep_running)
+	
 	if keep_running:
-		print(player.global_position.x - parent.global_position.x < 65 and player.global_position.x - parent.global_position.x > -65)
+	
 		if player.global_position.x - parent.global_position.x < 65 and player.global_position.x - parent.global_position.x > -65:
 			direction = 0
 		elif player.global_position.x - parent.global_position.x > 0:
@@ -30,15 +30,14 @@ func process_physics(delta:float) -> State:
 		movement *= direction
 	else:
 		movement = 0;
-	print(player.global_position.y)
-	print(movement)
+		
 	if movement != 0 or player.global_position.y < -5:
 		if movement < 0:
 			parent.get_node("AnimatedSprite2D").flip_h = true
 		else:
 			parent.get_node("AnimatedSprite2D").flip_h = false
 	else:
-		print("Idle State Started")
+		
 		return idle_state
 	
 	
